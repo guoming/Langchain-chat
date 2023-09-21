737d84fd5fff434f54d123d69dc3e6c8c5bae1ae
郭明 <zt7905@ztn.com>
2023-09-21 18:15:36 +0800

    chore:调整分词器

--- configs/__init__.py
+++
@@ -1,4 +0,0 @@
-from .model_config import *
-from .server_config import *
-
-VERSION = "v0.2.4"
--- server/model_workers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .zhipu import ChatGLMWorker
-from .minimax import MiniMaxWorker
-from .xinghuo import XingHuoWorker
-from .qianfan import QianFanWorker
